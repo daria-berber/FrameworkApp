78dd56c463cf2ce62341afa9ecdf1fc43550861f
daria-berber <loposhuk00@gmail.com>
2025-07-24 21:24:24 +0300

    Initial Commit

---
+++ Framew/FramewApp.swift
@@ -0,0 +1,17 @@
+//
+//  FramewApp.swift
+//  Framew
+//
+//  Created by Daria . on 24.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct FramewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FramewTests/FramewTests.swift
@@ -0,0 +1,17 @@
+//
+//  FramewTests.swift
+//  FramewTests
+//
+//  Created by Daria . on 24.07.2025.
+//
+
+import Testing
+@testable import Framew
+
+struct FramewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
